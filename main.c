eccaf487b2b5a6b01ffd2f3f988060e2b1f6a0db
Gia Bao <giabaotran912@gmail.com>
2021-10-12 20:50:49 +0700

    Moi lam phep cong

--- main.c
+++ main.c
@@ -1,11 +1,24 @@
 #include <stdio.h>
 
 // cộng trừ nhân chia
+// cộng
+void cong(int a, int b)
+{
+    int tong = a + b;
+    printf("Tong: %d", tong);
+}
 
 void main()
 {
     int a = 5, b = 10;
+    cong(a, b);
 }
 
 // 1. t sài git ở đây
 // 2. trỏ tới đâu
+// 3. đẩy dữ liệu lên git-hub:
+//                     - git add . (chấm có nghĩa là đẩy hết file dưới đây lên)
+//                     - git commit -m "" (để lại note)
+//                     - git push
+
+// git clone + link --> lấy 1 dự án có sẵn về máy
